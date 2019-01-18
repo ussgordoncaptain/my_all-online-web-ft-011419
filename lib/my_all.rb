7a4a1a581c33f12c2ada0262df1a5ff2d2e4f092
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-18 10:54:32 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,7 @@ def my_all?(collection)
   ii= 0 
   storage = []
   while (ii < collection.size)
-   final <<  yield(collection[ii])
+   storage <<  yield(collection[ii])
     i +=1
   end
  if storage.include?(false)
