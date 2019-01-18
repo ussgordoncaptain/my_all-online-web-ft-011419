8e6d7e3f459fdbecc4b33c8115c4890cf00482a1
Edmund Nelson <edmund.nelson.b@gmail.com>
2019-01-18 10:55:46 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
 require 'pry'
 
 def my_all?(collection)
-
+  ii= 0 
+  storage = []
+  while (ii < collection.length)
+   storage << yield(collection[ii])
+    ii +=1
+  end
+ if storage.include?(false)
+    return false
+  else
+    return true 
+  end
 end
